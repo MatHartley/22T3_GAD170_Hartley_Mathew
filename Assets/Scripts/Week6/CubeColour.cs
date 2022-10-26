4cef2df45b30e63e5960f5b318272a4aa1d4698f
MatHartley <115046381+MatHartley@users.noreply.github.com>
2022-10-26 12:00:22 +1100

    Week6
    week6 new pc

--- Assets/Scripts/Week6/CubeColour.cs
+++ Assets/Scripts/Week6/CubeColour.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace MathewAndDaniel
 {
-public class CubeColour : MonoBehaviour
-{
- 
-private GameObject cube;
-private Renderer cubeRenderer;
-private Color newColour;
+    public class CubeColour : MonoBehaviour
+    {
+
+        private GameObject cube;
+        private MeshRenderer cubeRenderer;
+        private Color newColour;
+        private float xDirection;
 
 
-    // Update is called once per frame
-    void Start()
-    {
-        cubeRenderer = cube.GetComponent<Renderer>();
-        cube.transform == (0,0,0);
-    }
-    
-    void Update()
-    {
-        if (cube.position.x <0)
+        // Update is called once per frame
+        void Start()
         {
-            cubeRenderer.material.SetColour(Colour.red);
+            cubeRenderer = GetComponent<MeshRenderer>();
+            //Debug.Log("checking");
+        }
+
+        void Update()
+        {
+            if (transform.position.x < 0)
+            {
+                cubeRenderer.material.color = Color.red;
+                //Debug.Log("colour should be red if cube is to the left");
+            }
+            else if (transform.position.x > 0)
+            {
+                cubeRenderer.material.color = Color.green;
+                //Debug.Log("colour should be green if cube is to the right");
+            }
         }
     }
-}
 }
